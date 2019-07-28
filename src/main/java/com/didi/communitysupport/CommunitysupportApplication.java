19d5e3ebc7627f2b903bb854657725dbe7fd29c7
lyj974755063 <lyj974755063@hotmail.com>
2019-07-28 10:48:24 +0800

    add mysql config

--- src/main/java/com/didi/communitysupport/CommunitysupportApplication.java
+++ src/main/java/com/didi/communitysupport/CommunitysupportApplication.java
@@ -2,6 +2,7 @@ package com.didi.communitysupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CommunitysupportApplication {
