a907e03348b9311a6c5f50d01a2d9a8f68e8bdfe
AllienKoco <jbyl1205@163.com>
2019-07-29 15:15:03 +0800

    更新接口的设计

---
+++ src/main/java/com/didi/communitysupport/controller/onebankController.java
@@ -0,0 +1,12 @@
+package com.didi.communitysupport.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *个人自检
+ * 获取检测题目	get	/getonebank					list(onebank)的json列表
+ * 提交测试题	post	/submitonebank	json（onebank）只存id和答案就可以				grade=数字 表示分数
+ */
+@Controller
+public class onebankController {
+}
