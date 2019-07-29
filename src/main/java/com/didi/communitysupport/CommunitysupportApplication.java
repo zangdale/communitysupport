e612880c8fc628079eb2c793643343e1316f2ee4
李志成 <980431578@qq.com>
2019-07-29 15:15:06 +0800

    first

--- src/main/java/com/didi/communitysupport/CommunitysupportApplication.java
+++ src/main/java/com/didi/communitysupport/CommunitysupportApplication.java
@@ -1,10 +1,12 @@
 package com.didi.communitysupport;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@MapperScan("com.didi.dao")
 public class CommunitysupportApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/didi/communitysupport/CommunitysupportApplicationTests.java
+++ src/test/java/com/didi/communitysupport/CommunitysupportApplicationTests.java
@@ -1,16 +1,23 @@
 package com.didi.communitysupport;
 
+
+import com.didi.communitysupport.dao.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@MapperScan("com.didi.dao")
 public class CommunitysupportApplicationTests {
-
+    @Autowired
+    UserMapper um;
     @Test
     public void contextLoads() {
+        System.out.println(um);
     }
 
 }
