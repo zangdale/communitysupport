4ab1ce458406b34de65e20e53ced59e90ae40cbd
AllienKoco <jbyl1205@163.com>
2019-07-29 14:58:52 +0800

    更新数据库驱动

--- src/main/java/com/didi/communitysupport/controller/loginControler.java
+++ src/main/java/com/didi/communitysupport/controller/loginControler.java
@@ -2,12 +2,14 @@ package com.didi.communitysupport.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class loginControler {
-    @RequestMapping("/login")
-    public String getlogin(){
 
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public String getlogin(){
         return "ok=0";
     }
 }
