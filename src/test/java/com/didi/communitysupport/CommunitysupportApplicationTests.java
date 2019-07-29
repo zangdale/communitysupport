e21d161cb5f6300e16d22bde1e5500209ff8d70b
李志成 <980431578@qq.com>
2019-07-29 16:40:47 +0800

    first

--- src/test/java/com/didi/communitysupport/CommunitysupportApplicationTests.java
+++ src/test/java/com/didi/communitysupport/CommunitysupportApplicationTests.java
@@ -2,12 +2,17 @@ package com.didi.communitysupport;
 
 
 import com.didi.communitysupport.dao.UserMapper;
+import com.didi.communitysupport.domain.UserEntity;
+import com.didi.communitysupport.service.LoginService;
+import com.didi.communitysupport.serviceimpl.LoginServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,7 +22,13 @@ public class CommunitysupportApplicationTests {
     UserMapper um;
     @Test
     public void contextLoads() {
-        System.out.println(um);
+        UserEntity user = new UserEntity();
+        user.setUAccount("1");
+        user.setUPasswd("1");
+        List<UserEntity> list = um.getByCondition(user);
+        System.out.println(list);
+
+
     }
 
 }
