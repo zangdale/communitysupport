6cb17d407e06375696141b4db7e278d4c6fe01c8
AllienKoco <jbyl1205@163.com>
2019-07-30 20:23:04 +0800

    更新url的安全监测

--- src/main/java/com/didi/communitysupport/controller/ArchivesController.java
+++ src/main/java/com/didi/communitysupport/controller/ArchivesController.java
@@ -3,14 +3,17 @@ package com.didi.communitysupport.controller;
 import com.didi.communitysupport.VO.ResultVO;
 import com.didi.communitysupport.domain.IllEntity;
 import com.didi.communitysupport.domain.UserEntity;
+import com.didi.communitysupport.enmu.ErrorEnum;
 import com.didi.communitysupport.service.GetArchivesService;
 import com.didi.communitysupport.service.LoginService;
 import com.didi.communitysupport.utils.ResultVOUtil;
+import com.didi.communitysupport.utils.VERBUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
@@ -28,8 +31,12 @@ public class ArchivesController {
     LoginService loginService;
 
     @GetMapping("/getarchives")
-    public ResultVO archives(HttpSession session){
-        UserEntity user = getArchivesService.getArchives(session);
+    public ResultVO archives(HttpServletRequest request){
+        UserEntity user = VERBUtil.getUserSession(request);
+        System.out.println(user);
+        if (user == null) {
+            return ResultVOUtil.error(ErrorEnum.E201);
+        }
         user= loginService.isLogin(user.getUAccount(),null);
         List<IllEntity> list = getArchivesService.illList(user.getUId());
         Map json = new HashMap();
--- src/main/java/com/didi/communitysupport/controller/CheckController.java
+++ src/main/java/com/didi/communitysupport/controller/CheckController.java
@@ -2,14 +2,17 @@ package com.didi.communitysupport.controller;
 
 import com.didi.communitysupport.VO.ResultVO;
 import com.didi.communitysupport.domain.UserEntity;
+import com.didi.communitysupport.enmu.ErrorEnum;
 import com.didi.communitysupport.service.CheckService;
 import com.didi.communitysupport.service.LoginService;
 import com.didi.communitysupport.utils.ResultVOUtil;
+import com.didi.communitysupport.utils.VERBUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -24,8 +27,12 @@ public class CheckController {
     @Resource
     LoginService loginService;
     @GetMapping("/getcheck")
-    public ResultVO check(HttpSession session){
-        UserEntity user = (UserEntity)session.getAttribute("user");
+    public ResultVO check(HttpServletRequest request){
+        UserEntity user = VERBUtil.getUserSession(request);
+        System.out.println(user);
+        if (user == null) {
+            return ResultVOUtil.error(ErrorEnum.E201);
+        }
         user= loginService.isLogin(user.getUAccount(),null);
         Map json = checkService.check(user.getUId());
 
