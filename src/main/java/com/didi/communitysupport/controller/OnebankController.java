ed759093533b32d7dbd59fd8ed501a9582e1b83a
AllienKoco <jbyl1205@163.com>
2019-07-30 17:19:03 +0800

    增加@Mapper的注解

--- src/main/java/com/didi/communitysupport/controller/OnebankController.java
+++ src/main/java/com/didi/communitysupport/controller/OnebankController.java
@@ -2,9 +2,11 @@ package com.didi.communitysupport.controller;
 
 import com.didi.communitysupport.VO.ResultVO;
 import com.didi.communitysupport.domain.AnswerEntity;
+import com.didi.communitysupport.domain.OnebankEntity;
 import com.didi.communitysupport.domain.QuestionEntity;
 import com.didi.communitysupport.domain.UserEntity;
 import com.didi.communitysupport.enmu.ErrorEnum;
+import com.didi.communitysupport.service.OneBankService;
 import com.didi.communitysupport.utils.ResultVOUtil;
 import com.didi.communitysupport.utils.VERBUtil;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.HashMap;
@@ -21,13 +24,20 @@ import java.util.Map;
 
 /**
  *个人自检
- * 获取检测题目	get	/getonebank					list(onebank)的json列表
+ * 获取检测题目	get	/getonebank					list(onebanks)的json列表
  * 提交测试题	post	/submitonebank	json（onebanks）只存id和答案就可以				grade=数字 表示分数
  */
 @RestController
 public class OnebankController {
 
+    @Resource
+    OneBankService oneBankService;
 
+    /**
+     * 获取检测题目	get	/getonebank					list(onebanks)的json列表
+     * @param request
+     * @return list(onebanks)的json列表
+     */
     @GetMapping("/getonebank")
     public ResultVO getOneBank(HttpServletRequest request) {
         Map json = new HashMap();
@@ -36,25 +46,26 @@ public class OnebankController {
         if (user == null) {
             return ResultVOUtil.error(ErrorEnum.E201);
         }
-        AnswerEntity answerEntity = new AnswerEntity();
-        answerEntity.setADate(new Date());
-
-        //answerEntity.setuName(user.getUName());
-        //adduserid
-        answerEntity.setAUserid(user.getUId());
-        boolean b = finddoctorService.addAnswerToQ(answerEntity);
-        if (!b) {
-            return ResultVOUtil.error(ErrorEnum.E500);
-        }
-        QuestionEntity q = finddoctorService.getQuestion(qid);
-        json.put("question", q);
-        List<AnswerEntity> answerEntityList = finddoctorService.getAnswersByQuestion(qid);
-        json.put("answers", answerEntityList);
+        List<OnebankEntity> onebankEntities =  oneBankService.getOneBankList();
+        json.put("onebanks", onebankEntities);
         return ResultVOUtil.success(json);
     }
+
+    /**
+     * 提交测试题	post	/submitonebank	json（onebanks）只存id和答案就可以				grade=数字 表示分数
+     * @param request
+     * @return grade=数字 表示分数
+     */
     @PostMapping("/submitonebank")
-    public ResultVO submitOneBank(HttpServletRequest request) {
+    public ResultVO submitOneBank(@RequestParam("onebanks") String onebanks ,HttpServletRequest request) {
         Map json = new HashMap();
+        UserEntity user = VERBUtil.getUserSession(request);
+        System.out.println(user);
+        if (user == null) {
+            return ResultVOUtil.error(ErrorEnum.E201);
+        }
+        int grade=oneBankService.SubmitOneBank(onebanks);
+        json.put("grade",grade);
         return ResultVOUtil.success(json);
     }
 }
